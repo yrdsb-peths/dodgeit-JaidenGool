36baf2266d7a0e8763e8992ea7c41a7014df3f00
Jaiden Gool <349162586@gapps.yrdsb.ca>
2024-04-23 16:38:23 -0400

    Add SadFace

--- Apple.java
+++ Apple.java
@@ -11,6 +11,13 @@ public class Apple extends Actor
         {
             resetApple();
         }
+        
+        if(isTouching(Hero.class))
+        {
+            SadFace sadFace = new SadFace();
+            getWorld().addObject(sadFace, 300, 200);
+            getWorld().removeObject(this);
+        }
     }
     
     public void resetApple()
